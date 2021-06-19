e47ddaa562a81e18f38ad6303d2790549ec1761f
kkn1125 <xpfjrnseks1@naver.com>
2021-06-20 02:44:12 +0900

    1.1ver

--- assets/dist/js/main.js
+++ assets/dist/js/main.js
@@ -1,13 +1,14 @@
 var path = window.location.href;
-
-console.log(window.location.hostname);
-console.log(window.location.pathname);
-
+const pathnames = "/portfolio/gitrepo/";
+var now = window.location.pathname.substring(pathnames.length-1);
 console.log(path);
+// console.log(window.location.hostname);
+console.log(now);
+
 
 
 $.ajax({ // header
-    url:"data/header",
+    url:`${pathnames}data/header`,
     // data:{},
     method:"get",
     dataType:"text",
@@ -18,18 +19,29 @@ $.ajax({ // header
     error:function(request, status, error){
         console.error(error);
     }
-})
+});
 
-$.ajax({ // body
-    url:"data/test.txt",
+$(document).ready(function(e){
+    let currentPath = now.split("/")[now.split("/").length-1].split(".")[0];
+    document.title = `Kims ${currentPath}`; // 타이틀 페이지별 자동 변경
+    $("a[data-path]").each(function(num,tag){
+        console.log($(tag));
+        let hr = `${pathnames}${$(tag).attr("data-path")}`;
+        $(tag).attr("href",hr);
+        console.log($(tag).attr("href")!=null);
+    });
+});
+
+$.ajax({
+    url:`${pathnames}data/footer`,
     // data:{},
     method:"get",
     dataType:"text",
     success:function(data){
         console.log('success');
-        $('[data-role="body"]').html(data);
+        $('[data-role="footer"]').html(data);
     },
     error:function(request, status, error){
         console.error(error);
     }
-})
+});
---
+++ assets/dist/js/sunedit.js
@@ -0,0 +1,27 @@
+const editor = SUNEDITOR.create((document.getElementById('content') || 'content'),{
+    // All of the plugins are loaded in the "window.SUNEDITOR" object in dist/suneditor.min.js file
+    // Insert options
+    // Language global object (default: en)
+    buttonList: [
+        ['undo', 'redo'],
+        ['font', 'fontSize', 'formatBlock'],
+        ['paragraphStyle', 'blockquote'],
+        ['bold', 'underline', 'italic', 'strike', 'subscript', 'superscript'],
+        ['fontColor', 'hiliteColor', 'textStyle'],
+        ['removeFormat'],
+        '/', // Line break
+        ['outdent', 'indent'],
+        ['align', 'horizontalRule', 'list', 'lineHeight'],
+        ['table', 'link', 'image', 'video', 'audio' /** ,'math' */], // You must add the 'katex' library at options to use the 'math' plugin.
+        /** ['imageGallery'] */ // You must add the "imageGalleryUrl".
+        ['fullScreen', 'showBlocks', 'codeView'],
+        ['preview', 'print'],
+        ['save', 'template']
+    ],
+    lang: SUNEDITOR_LANG['ko'],
+    placeholder: 'test'
+});
+editor.onImageUpload = function (targetElement, index, state, info, remainingFilesCount, core) {
+    console.log(`targetElement:${targetElement}, index:${index}, state('create', 'update', 'delete'):${state}`)
+    console.log(`info:${info}, remainingFilesCount:${remainingFilesCount}`)
+}
