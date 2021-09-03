453376e90e69ae33a755694e9f1de45182570bc1
kkn1125 <xpfjrnseks1@naver.com>
2021-09-03 21:30:48 +0900

    update post

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,3 +3,20 @@ console.log(window.localStorage.getItem("dark"))
 if(window.localStorage.getItem("dark")=="Y"){
     document.body.classList.add('dark');
 }
+
+window.addEventListener('load',()=>{
+    for(let i=1; i<=6; i++){
+        let h = document.querySelectorAll(`h${i}`);
+        h.forEach(x=>{
+            let id = x.getAttribute("id");
+            if(id){
+                let a = document.createElement('a');
+                a.href=`#${id}`;
+                a.classList.add('linkpop');
+                a.innerHTML = `🔗`;
+                x.prepend(a);
+                console.log(x)
+            }
+        });
+    }
+})
